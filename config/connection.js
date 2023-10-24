d27fe094a40a3610744f282709ad016140143dad
Thinker92 <vilsonapo@gmail.com>
2023-10-23 22:10:50 -0400

    Added Reaction schema to thought model, and tested server/database connection

--- config/connection.js
+++ config/connection.js
@@ -2,4 +2,8 @@ const { connect, connection } = require('mongoose');
 
 connect('mongodb://127.0.0.1:27017/mongoverse');
 
+connection.on('connected', () => {
+    console.log('MongoDB successfully connected!');
+});
+
 module.exports = connection;
