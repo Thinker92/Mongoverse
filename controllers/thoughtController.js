8eb65dccaace3be7532624b040a63a87d64b2e80
Thinker92 <vilsonapo@gmail.com>
2023-11-06 22:07:09 -0500

    Setup CRUD functions in thoughtController.js

---
+++ controllers/thoughtController.js
@@ -0,0 +1,47 @@
+const Thought = require('../models/Thought');
+const User = require('../models/User');
+
+// Get all thoughts
+const getAllThoughts = async (req, res) => {
+    
+};
+
+// Get a single thought by id
+const getThoughtById = async (req, res) => {
+    
+};
+
+// Add a new thought
+const addThought = async (req, res) => {
+    
+};
+
+// Update a thought by id
+const updateThought = async (req, res) => {
+    
+};
+
+// Remove a thought by id
+const removeThought = async (req, res) => {
+    
+};
+
+// Add a reaction to a thought
+const addReaction = async (req, res) => {
+
+};
+
+// Remove a reaction from a thought
+const removeReaction = async (req, res) => {
+
+};
+
+module.exports = {
+  getAllThoughts,
+  getThoughtById,
+  addThought,
+  updateThought,
+  removeThought,
+  addReaction,
+  removeReaction
+};
