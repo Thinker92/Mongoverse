eddf181580801de5bda98e57689bc9a3c0980357
Thinker92 <vilsonapo@gmail.com>
2023-11-06 22:11:05 -0500

    Updated GET routes for thoughtController

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,12 +3,25 @@ const User = require('../models/User');
 
 // Get all thoughts
 const getAllThoughts = async (req, res) => {
-    
+    try {
+        const thoughts = await Thought.find({});
+        res.status(200).json(thoughts);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };
 
 // Get a single thought by id
 const getThoughtById = async (req, res) => {
-    
+    try {
+        const thought = await Thought.findById(req.params.id);
+        if (!thought) {
+            return res.status(404).json({ message: 'No thought found with this id!' });
+        }
+        res.status(200).json(thought);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };
 
 // Add a new thought
